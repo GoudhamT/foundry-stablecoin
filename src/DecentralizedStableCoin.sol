4408d3f7e1a63b1ab7296f8a973baf121f140c63
GoudhamT <goudhamvel.88@gmail.com>
2026-01-30 20:19:39 -0800

    mint and burn function

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -36,5 +36,35 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  */
 
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    /*Errors */
+    error DecentralizedStableCoin__AmountCannotBeZero();
+    error DecentralizedStableCoin__AmountExceedsBalance();
+    error DecentralizedStableCoin__InvalidAddress();
+
     constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountCannotBeZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__AmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) public onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__InvalidAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountCannotBeZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
 }
